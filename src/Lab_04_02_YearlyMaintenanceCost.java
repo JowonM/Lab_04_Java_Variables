6b9ddd9674e08cd94676ec60cde3c249daa18a15
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-05 19:12:20 -0500

    “Project init”

--- src/Lab_04_02_YearlyMaintenanceCost.java
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -1,5 +1,13 @@
 public class Lab_04_02_YearlyMaintenanceCost {
     public static void main(String[] args) {
+        double s1 = 10.25;
+        double s2 = 20.50;
+        double s3 = 30.75;
+        double s4 = 40.15;
+        double sTotal = s1 + s2 + s3 + s4;
+        System.out.println("The separate season totals are: "+ s1 +  " / " + s2 + " / " + s3 + " / " + s4) ;
+        System.out.println("The total maintenance cost for the four seasons will be: "+sTotal) ;
+
 
     }
 
