86a65fb89c59246869c6cdef60e8a2e6d2763d11
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-05 19:27:32 -0500

    “Project init”

--- src/Lab_04_03_MonthyCCBalance.java
+++ src/Lab_04_03_MonthyCCBalance.java
@@ -1,5 +1,19 @@
 public class Lab_04_03_MonthyCCBalance {
     public static void main(String[] args) {
+        double startingBal = 5000.00;
+        double intRate = 1.17;
+        int count = 2;
+        double bal;
+        double intTotal;
+
+        while(count>0){
+            bal = (startingBal*intRate);
+            intTotal = bal-startingBal;
+            count--;
+            System.out.println("These are the two interest totals (not including your balance): "+ intTotal);
+            startingBal = bal;
+
+        }
 
     }
 }
