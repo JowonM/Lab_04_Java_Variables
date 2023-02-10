a2709719ce4c82c48fb4beb903ae2a830dbb90e3
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-09 23:41:29 -0500

    “Project init”

--- src/Lab_05_04_TheaterKiosk.java
+++ src/Lab_05_04_TheaterKiosk.java
@@ -1,2 +1,10 @@
 public class Lab_05_04_TheaterKiosk {
+    public static void main(String[] args) {
+        System.out.println("Please enter your age");
+        double age = 21.4;
+        if (age >= 21){
+            System.out.println("You will receive a wristband");
+        }
+
+    }
 }
