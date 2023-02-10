aadf2e3fb746ddef6a57b66f1b13ebae5a035393
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-09 22:30:32 -0500

    “Project init”

---
+++ src/Lab_05_01_ShipCostCalculator.java
@@ -0,0 +1,21 @@
+public class Lab_05_01_ShipCostCalculator {
+    public static void main(String[] args) {
+        double total;
+        double ship = 1.02;
+        double price = 77;
+
+        if (price < 100)
+        {
+            System.out.println("Your total cost will be: " + price);
+        }
+        else
+        {
+            total = price * ship;
+            System.out.println("Your total cost will be: " + price);
+
+        }
+
+    }
+}
+
+
