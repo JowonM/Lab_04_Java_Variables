ab5f5acf987bb94d2b1c8d1358446860fb2defa0
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-11 13:47:10 -0500

    “Project init”

--- src/Lab_05_05_NumCompare.java
+++ src/Lab_05_05_NumCompare.java
@@ -1,19 +1,29 @@
 public class Lab_05_05_NumCompare {
     public static void main(String[] args) {
-        double inputA = 100;
-        double inputB = 200;
+
+
         //faking a scanner object here for the inputs//
         System.out.println("Please enter your first number");
+        int inputA = 5;
         System.out.println("Please enter your second number");
-        if(inputA > inputB){
-            System.out.println("Your second input is smaller");
-        }
-        else if (inputB > inputA) {
-            System.out.println("Your first input is smaller than");
-        }
-        else if (inputB == inputA) {
-            System.out.println("Your inputs are equal");
+        int inputB = 200;
+        //boolean isA = Character.isDigit(inputA);//
+
+        if(((inputA & inputB) >= 0) || ((inputA & inputB) < 0)){
+            if(inputA > inputB){
+                System.out.println("Your second input is smaller");
+            }
+            else if (inputB > inputA) {
+                System.out.println("Your first input is smaller than");
+            }
+            else if (inputB == inputA) {
+                System.out.println("Your inputs are equal");
+            }
+            }
+            else {
+                System.out.println("Please enter a valid digit");
+            }
+
 
         }
     }
-}
