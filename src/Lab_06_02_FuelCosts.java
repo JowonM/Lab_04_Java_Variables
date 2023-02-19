4871ddc09e34869da362405d6722fd44502db214
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-19 16:07:18 -0500

    a

--- src/Lab_06_02_FuelCosts.java
+++ src/Lab_06_02_FuelCosts.java
@@ -1,6 +1,46 @@
 import java.util.Scanner;
 public class Lab_06_02_FuelCosts {
     public static void main(String[] args) {
+        //initializing my variables//
+        double gallons = 0.0;
+        double fuelEff = 0.0;
+        double price = 0.0;
+        //first scanner object//
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter the amount of gallons");
+        //code to check if the value entered was a number//
+        if (in.hasNextDouble()){
+            gallons = in.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid number");
+        }
+
+        System.out.println("please enter your fuel efficiency");
+        //code to check if the value entered was a number and second scanner object//
+        Scanner inTwo = new Scanner(System.in);
+        if (inTwo.hasNextDouble()){
+            fuelEff = inTwo.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid number");
+        }
+
+        System.out.println("please enter the price per gallon of gas");
+        //code to check if the value entered was a number and third scanner object//
+        Scanner inThree = new Scanner(System.in);
+        if (inThree.hasNextDouble()){
+            price = inThree.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid number");
+        }
+        //code to find the cost of 100 miles and the current amount of miles you could travel//
+        double costPer100 = (100/fuelEff)*price;
+        double currentAmount = gallons * fuelEff;
+
+        System.out.println("The cost for 100 miles is: " + costPer100 + " $");
+        System.out.println("You can currently go " + currentAmount + " miles");
 
     }
 }
--- src/Lab_06_03_RectangleInfo.java
+++ src/Lab_06_03_RectangleInfo.java
@@ -1,6 +1,41 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class Lab_06_03_RectangleInfo {
     public static void main(String[] args) {
+        double length = 0;
+        double width = 0;
+        System.out.println("Please input a side length");
+        // scanner object//
+        Scanner in = new Scanner(System.in);
+        //if statement to check if there is a valid input//
+        if (in.hasNextDouble()){
+            length = in.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid number!");
+        }
+
+        System.out.println("Please input a side width");
+        // scanner object//
+        Scanner inTwo = new Scanner(System.in);
+        //if statement to check if there is a valid input//
+        if (inTwo.hasNextDouble()){
+            width = inTwo.nextDouble();
+        }
+        else{
+            System.out.println("Please enter a valid number!");
+        }
+        //variables for the output//
+        double area = length * width;
+        double perimeter = (length * 2) + (width * 2);
+        //imported Math class for the power function//
+        double diagonal = Math.pow(((length*length) + (width*width)),0.5);
+
+        System.out.println ("This is the area: " + area);
+        System.out.println ("This is the perimeter: " + perimeter);
+        System.out.println ("This is the length of the diagonal: " + diagonal);
+
+
 
     }
 }
