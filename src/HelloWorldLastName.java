ef86a336531dba9b1c737b823f1876ff013a7cb1
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-23 17:19:36 -0500

    a

---
+++ src/HelloWorldLastName.java
@@ -0,0 +1,9 @@
+public class HelloWorldLastName {
+    public static void main(String[] args) {
+
+        String helloMessage = "Hello World! Hello Jowon M Briggs!";
+        System.out.println(helloMessage);
+
+
+    }
+}
