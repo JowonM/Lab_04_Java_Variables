01161702fbbef146a09a77763ca1197d81f12bf4
Jowon M. Briggs <briggsjm@mail.uc.edu>
2023-02-26 22:07:47 -0500

    a

--- src/GetLoopy.java
+++ src/GetLoopy.java
@@ -1,9 +1,47 @@
 public class GetLoopy {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= 5; j++){
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 20; j++){
                 System.out.print("*");
             }
+
             System.out.println();
         }
-}}
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <= 0; j++){
+                System.out.print("        ****");
+            }
+
+            System.out.println();
+        }
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 0; j++){
+                System.out.print("**      ****");
+            }
+
+            System.out.println();
+        }
+        for (int i = 0; i <= 0; i++) {
+            for (int j = 0; j <= 0; j++){
+                System.out.print(" **********");
+            }
+
+            System.out.println();
+        }
+        for (int i = 0; i <= 0; i++) {
+            for (int j = 0; j <= 0; j++){
+                System.out.print("  ********");
+            }
+
+            System.out.println();
+        }
+        for (int i = 0; i <= 0; i++) {
+            for (int j = 0; j <= 0; j++){
+                System.out.print("   ******");
+            }
+
+            System.out.println();
+        }
+
+
+    }}
